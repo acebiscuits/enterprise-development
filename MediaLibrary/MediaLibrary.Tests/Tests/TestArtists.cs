d6503cae078e9e5fdf44f68582e5d06bdba19b30
Acebiscuits <27roter72@gmail.com>
2024-10-13 18:37:14 +0400

    upd: tests updated to use services and in-memory repositories

--- MediaLibrary/MediaLibrary.Tests/Tests/TestArtists.cs
+++ MediaLibrary/MediaLibrary.Tests/Tests/TestArtists.cs
@@ -1,5 +1,4 @@
 ﻿using MediaLibrary.Tests.Fixtures;
-
 namespace MediaLibrary.Tests.Tests;
 
 /// <summary>
@@ -16,33 +15,33 @@ public class TestArtists(MediaLibraryFixture fixture) : IClassFixture<MediaLibra
     [Fact]
     public void TestShowInfoAboutAllArtists()
     {
-        var query = _fixture.Artists.ToList();
+        var artists = _fixture.ArtistService.GetAll().ToList();
 
-        Assert.Equal(7, query.Count);
+        Assert.Equal(7, artists.Count);
 
         Assert.Equal(
             [
                 "Jon Bon Jovi", "Bob Seger", "Jay Kay",
-                "Roger Daltrey", "Lionel Richie", "Cliff Richard", "James Blunt" 
+                "Roger Daltrey", "Lionel Richie", "Cliff Richard", "James Blunt"
             ],
-            query.Select(q => q.Name).ToList());
+            artists.Select(q => q.Name).ToList());
 
         Assert.Equal(
             [
                 "У него есть кожаная куртка с дюжиной застежек", "Его не заткнуть, а его смех похож на скрежет бетономешалки",
                 "Он как-то говорил, что не смог бы стать раллийным гонщиком", "Самый быстрый человек с билетом на автобус",
                 "Он часто оказывался на обочине и пытался открыть правую дверь", "Его усилия разогнаться иссякли на 130 километрах в час",
-                "Оденажды он спел о чем-то там в море" 
+                "Оденажды он спел о чем-то там в море"
             ],
-            query.Select(q => q.Description).ToList());
+            artists.Select(q => q.Description).ToList());
 
         Assert.Equal([ [ 1, 2 ], [ 3, 4 ], [ 5 ],
             [ 6, 7 ], [ 8 ], [ 9, 10 ], [ 11, 12 ] ],
-            query.Select(q => q.AlbumIds).ToList());
+            artists.Select(q => q.AlbumIds).ToList());
 
         Assert.Equal([ [ 1, 2 ], [ 3 ], [ 4 ],
             [ 3 ], [ 4 ], [ 4, 5 ], [ 3, 5 ] ],
-            query.Select(q => q.GenreIds).ToList());
+            artists.Select(q => q.GenreIds).ToList());
 
     }
 
@@ -52,21 +51,21 @@ public class TestArtists(MediaLibraryFixture fixture) : IClassFixture<MediaLibra
     [Fact]
     public void TestAllSongsInAlbumOrderedByNumber()
     {
-        var query = _fixture.Songs.Where(a => a.AlbumName == "Stronger").OrderBy(a => a.NumberInAlbum).ToList();
+        var songs = _fixture.SongService.GetAll().Where(a => a.AlbumName == "Stronger").OrderBy(a => a.NumberInAlbum).ToList();
 
-        Assert.Equal(6, query.Count);
+        Assert.Equal(6, songs.Count);
 
         Assert.Equal([ "Stronger Than That", "Who's In Love", "The Best Of Me",
             "Clear Blue Skies", "Lean On You", "Keep Me Warm" ],
-            query.Select(q => q.Name).ToList());
+            songs.Select(q => q.Name).ToList());
 
         Assert.Equal([ TimeSpan.FromSeconds(281), TimeSpan.FromSeconds(270),
             TimeSpan.FromSeconds(250), TimeSpan.FromSeconds(174),
             TimeSpan.FromSeconds(302), TimeSpan.FromSeconds(264) ],
-            query.Select(q => q.Duration).ToList());
+            songs.Select(q => q.Duration).ToList());
 
-        Assert.Equal([ 1, 2, 3, 4, 5, 6 ],
-            query.Select(q => q.NumberInAlbum).ToList());
+        Assert.Equal([1, 2, 3, 4, 5, 6],
+            songs.Select(q => q.NumberInAlbum).ToList());
 
     }
 
@@ -76,17 +75,17 @@ public class TestArtists(MediaLibraryFixture fixture) : IClassFixture<MediaLibra
     [Fact]
     public void TestAlbumsInfoWithSongsCountInCertainYear()
     {
-        var query = _fixture.Albums.Where(q => q.ReleaseDate == 1980).ToList();
+        var query = _fixture.AlbumService.GetAll().Where(q => q.ReleaseDate == 1980).ToList();
 
         Assert.Equal(2, query.Count);
 
-        Assert.Equal([ "Against The Wind", "Nine Tonight" ],
+        Assert.Equal(["Against The Wind", "Nine Tonight"],
             query.Select(q => q.Title).ToList());
 
-        Assert.Equal([ 2, 3 ],
+        Assert.Equal([2, 3],
             query.Select(q => q.ArtistId).ToList());
 
-        Assert.Equal([ 4, 4 ],
+        Assert.Equal([4, 4],
             query.Select(q => q.SongIds.Count).ToList());
     }
 
@@ -96,7 +95,7 @@ public class TestArtists(MediaLibraryFixture fixture) : IClassFixture<MediaLibra
     [Fact]
     public void TestTopFiveAlbumsByDuration()
     {
-        var query = _fixture.SongsForTopFiveAlbums.GroupBy(q => q.AlbumName)
+        var query = _fixture.songsForTopFiveAlbumsRepository.GetAll().GroupBy(q => q.AlbumName)
             .Select(a => new { AlbumName = a.Key, TotalDuration = a.Aggregate(TimeSpan.Zero, (total, song) => total + song.Duration) })
             .OrderByDescending(d => d.TotalDuration).Take(5).ToList();
 
@@ -112,13 +111,20 @@ public class TestArtists(MediaLibraryFixture fixture) : IClassFixture<MediaLibra
     [Fact]
     public void TestArtistsWithMaxAlbumsCount()
     {
-        var query = _fixture.Albums.GroupBy(a => a.ArtistId)
-                    .Select(g => new { ArtistId = g.Key, AlbumCount = g.Count() })
-                    .OrderByDescending(g => g.AlbumCount)
-                    .FirstOrDefault();
+        var query = _fixture.ArtistService
+        .GetAll()
+        .Where(a => a.AlbumIds.Count == _fixture.infoAboutAllArtistsAndWithMaxAlbumsRepository.GetAll().Max(a => a.AlbumIds.Count))
+        .Select(group => new
+        {
+            Id = group.Id,
+            Count = group.AlbumIds.Count,
+        }).ToList();
+        var maxAlbumCount = _fixture.infoAboutAllArtistsAndWithMaxAlbumsRepository
+        .GetAll()
+        .Max(a => a.AlbumIds.Count);
 
-        Assert.Equal(2, query!.ArtistId);
-        Assert.Equal(3, query.AlbumCount);
+        Assert.Equal([1, 2, 4, 6, 7], query!.Select(q => q.Id).ToList());
+        Assert.All(query, q => Assert.Equal(maxAlbumCount, q.Count));
     }
 
     /// <summary>
@@ -127,8 +133,8 @@ public class TestArtists(MediaLibraryFixture fixture) : IClassFixture<MediaLibra
     [Fact]
     public void TestMaxMinMedAlbumsDurationInfo()
     {
-        var query = _fixture.SongsInOneAlbum.GroupBy(n => n.AlbumName)
-            .Select(g => new { AlbumName = g.Key, TotalDuration = g.Sum(s => s.Duration.TotalSeconds) })
+        var query = _fixture.MaxMinMedAlbumsDurationInfoService.GetAll().GroupBy(n => n.AlbumName)
+            .Select(g => new { AlbumName = g.Key, TotalDuration = g.Sum(s => s.Duration.HasValue ? s.Duration.Value.TotalSeconds : 0) })
             .OrderByDescending(d => d.TotalDuration)
             .ToList();
 
