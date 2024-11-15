1751d304ecc6459543be01594acaf21fd20f672a
Acebiscuits <27roter72@gmail.com>
2024-11-16 00:02:43 +0400

    add: client api

---
+++ MediaLibrary/MediaLibrary.Client/Program.cs
@@ -0,0 +1,14 @@
+using MediaLibrary.Client;
+using MediaLibrary.Client.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp =>
+    new MediaLibraryClient("http://localhost:5071", new HttpClient()));
+
+
+await builder.Build().RunAsync();
